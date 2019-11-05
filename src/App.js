4e80d2a86e5ca69a4cbcb094069e37fd136928ad
Jhon Arias <jhonarmont@hotmail.com>
2019-11-05 03:51:27 -0500

    Update app component.
    Divide the main page view into navbar, layour and content.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Sidebar from './components/sidebar';
 import Navbar from './components/Navbar';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 
 function App() {
   return (
     < div className = "App container-fluid" >
-      <div className="row">
+      <div>
         <Navbar />
-        <Sidebar />
+      </div>
+      <div className="row layout">
+        < div className = "col-md-3 categories-container" >
+          < Sidebar / >
+        </div>
+        <div className="col-md-9">
+          {/* <Content /> */}
+        </div>
       </div>
     </div>
   );
