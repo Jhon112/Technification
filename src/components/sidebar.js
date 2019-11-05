16430786c2310473f81531e8df1e263814966cb4
Jhon Arias <jhonarmont@hotmail.com>
2019-11-05 03:51:59 -0500

    Change component name and remove class from div

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import './styles/sidebar.css'
 
-export default class Navbar extends React.Component {
+export default class Sidebar extends React.Component {
 
     render () {
         return (
-            < div className = "col-md-3 categories-container" >
+            < div>
                 <h2>Categorias</h2>
                 < ul className = "nav .nav-pills nav-stacked " >
                     <li class="active"><a href="/">Home</a></li>
