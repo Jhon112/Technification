f9de1adcc684a97076d1c074f198f022fdbe46d4
Jhon Arias <jhonarmont@hotmail.com>
2019-11-05 02:21:36 -0500

    Update app component.
    Import Sidebar component and bootstrap style.
    
    Create a container and created a row for the Sidebar and content.
    
    Navbar will be a different row above Sidebar and content row

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import Sidebar from './components/sidebar';
+import Navbar from './components/Navbar';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < div className = "App container-fluid" >
+      <div className="row">
+        <Navbar />
+        <Sidebar />
+      </div>
     </div>
   );
 }
